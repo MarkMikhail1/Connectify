2e89fde57802addef425531da3a11ba7e914232b
mmik0003 <markmikhail4@gmail.com>
2023-01-19 05:36:15 +1100

    Add main react component scene templates and redux state functionality

---
+++ client/src/state/index.js
@@ -0,0 +1,49 @@
+import { UpdateDisabled } from '@mui/icons-material';
+import { createSlice } from '@reduxjs/toolkit';
+import { act } from 'react-dom/test-utils';
+
+const initialState = {
+    mode: "light",
+    user: null,
+    token: null,
+    posts: [],
+
+};
+
+export const authSlice = createSlice ({
+    name: "auth",
+    initialState,
+    reducers: {
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark": "light";
+        },
+        setLogin: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+        setLogout: (state) => {
+            state.user = null;
+            state.token = null;
+        },
+        setFriends: (state, action) => {
+            if(state.user) {
+                state.user.friends = action.payload.friends;
+            } else {
+                console.error(" user's friends are non-existent :(")
+            }
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts;
+        },
+        setPost: (state, action) => {
+            const updatePosts = state.posts.map((post) => {
+                if(post._id === action.payload.post_id) return action.payload.post;
+                return post;
+            });
+            state.posts = updatedPosts;
+        }
+    }
+})
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions;
+export default authSlice.reducer;
