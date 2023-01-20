02bc5ba1f378d2ddaad964231dbe652eb3e31383
mmik0003 <markmikhail4@gmail.com>
2023-01-21 02:39:34 +1100

    Add NavBar react components with Mobile and Desktop modes

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,5 +1,15 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Navbar from "scenes/navBar";
+
 const ProfilePage = () => {
-    return <div>profilepage</div>
+    return (
+        <Box>
+            <Navbar />
+        </Box>
+    )
 };
 
 export default ProfilePage;
